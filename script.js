0543d2cc0f754403712f6f3910c965df35878814
dilora-gif <sharipovadilora@gmail.com>
2025-09-10 23:09:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,11 @@ let html5QrcodeScanner = new Html5QrcodeScanner(
   "qr-reader", { fps: 10, qrbox: 250 });
 html5QrcodeScanner.render(onScanSuccess, onScanFailure);
     function loginPayme() {
-      window.location.href = "https://payme.uz/login"; 
+       window.location.href = "https://payme.uz/home/register"
     }
 
     function loginClick() {
-      window.location.href = "https://my.click.uz/"; 
+       window.location.href = "https://my.click.uz/#/registration";
     }
+
 
